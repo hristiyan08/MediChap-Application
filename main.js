1060870cf5eb893354389aa258cac545a87696e7
biology1234 <hristianjordanov08@gmail.com>
2024-10-13 11:22:38 +0300

    ADS

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 // Function to generate a random 8-digit number
-function generateRandom8DigitNumber() {
-    return Math.floor(10000000 + Math.random() * 90000000);
-}
 
-// Function to get values and generate random number
-export function getUserData() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const randomNum = generateRandom8DigitNumber();
-
-    return { name, email, randomNum }; // Return an object with all data
-}
+    const code = Math.floor(10000000 + Math.random() * 90000000);
+    document.getElementById("code").innerHTML = code;
